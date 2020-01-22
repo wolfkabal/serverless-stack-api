71f35ea06c45b4099089985121f59b0fa701a445
Tim D <wolfkabal@gmail.com>
2020-01-22 00:13:27 -0500

    code cleanup

--- billing.js
+++ billing.js
@@ -3,23 +3,22 @@ import { calculateCost } from "./libs/billing-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-    const {storage, source} = JSON.parse(event.body);
-    const amount = calculateCost(storage);
-    const description = "Scratch charge";
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
 
-    // Load our secret key from the env var
-    const stripe = stripePackage(process.env.stripeSecretKey);
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
 
-    try {
-        await stripe.charges.create({
-           source,
-           amount,
-           description,
-           currency: "usd"
-        });
-        return success({ status: true});
-    } catch(e) {
-        console.log(e);
-        return failure({ status: false});
-    }
+  try {
+    await stripe.charges.create({
+      source,
+      amount,
+      description,
+      currency: "usd"
+    });
+    return success({ status: true });
+  } catch (e) {
+    return failure({ message: e.message });
+  }
 }
